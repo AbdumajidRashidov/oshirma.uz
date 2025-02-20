e5d01e6e9fea54926a20101ffe17b9885ee98e09
Abdumajid  Rashidov <abdumajidrashidov44@gmail.com>
2025-02-20 01:28:44 +0000

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,51 @@
-import { resolve } from "path"
+import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
   alias: {
-      "@": resolve(__dirname, "/"),
+    "@": resolve(__dirname, "/"),
   },
 
   app: {
-      head: {
-          meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
-          title: "Agon - Multipurpose Agency TailwindCSS NuxtJS Template",
-          script: []
-      }
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      title: "Oshirma.uz - Isrofgarchilikni emas, daromadni oshiring!",
+      script: [],
+    },
   },
 
   css: [
-      'swiper/css',
-      'swiper/css/navigation',
-      'swiper/css/pagination',
-      'swiper/css/free-mode',
-      'swiper/css/thumbs',
-      "~/public/assets/styles/app.min.css",
-      "~/public/assets/styles/tailwind.min.css",
+    "swiper/css",
+    "swiper/css/navigation",
+    "swiper/css/pagination",
+    "swiper/css/free-mode",
+    "swiper/css/thumbs",
+    "~/public/assets/styles/app.min.css",
+    "~/public/assets/styles/tailwind.min.css",
   ],
 
   modules: [
-      [
-          "@nuxtjs/google-fonts",
-          {
-              families: {
-                  Chivo: {
-                      wght: [400, 700, 900]
-                  },
-                  "Noto+Sans": {
-                      wght: [400, 500, 600, 700,800]
-                  },
-                  download: true,
-                  inject: true
-              }
-          }
-      ],
-      // "swiper"
-      // "vue-youtube-embed"
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Chivo: {
+            wght: [400, 700, 900],
+          },
+          "Noto+Sans": {
+            wght: [400, 500, 600, 700, 800],
+          },
+          download: true,
+          inject: true,
+        },
+      },
+    ],
+    // "swiper"
+    // "vue-youtube-embed"
   ],
 
   compatibilityDate: "2025-02-20",
-})
+});
