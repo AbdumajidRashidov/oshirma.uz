3939536c8554b982edc74561439a072e831a8566
Abdumajid  Rashidov <abdumajidrashidov44@gmail.com>
2025-02-20 12:10:06 +0000

    vercel analitics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,8 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 <template>
+    <Analytics />
     <div class="wrapper mx-auto text-gray-900 font-normal max-w-[1320px]">
         <!-- <ElementsVideoPopup /> -->
         <LayoutHeader1 />
