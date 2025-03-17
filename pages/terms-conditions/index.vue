531510742c46403a3e5a5c6e7934a33bc1ea6466
Abdumajid  Rashidov <abdumajidrashidov44@gmail.com>
2025-03-17 17:51:27 +0500

    fixed: privacy-policy and terms-conditions

---
+++ pages/terms-conditions/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="px-[12px] md:px-[36px] xl:px-0 mt-[70px] mx-auto max-w-[1173px]">
+        <div class="grid grid-cols-1 md:grid-cols-[168px_1fr_55px] mb-[61px]">
+            <div></div>
+            <h1
+                class="font-chivo text-[35px] leading-[44px] sm:text-[46px] sm:leading-[52px] md:text-heading-1 font-extrabold text-center md:ml-[78px] md:mr-[50px]">
+                Terms & Conditions</h1>
+            <div></div>
+        </div>
+        <div
+            class="grid text-gray-500 grid-cols-[1fr_55px] md:grid-cols-[168px_1fr_55px] text-[14px] leading-[25px] md:text-[18px] md:leading-[32px]">
+
+            <div class="content order-last col-span-2 md:ml-[78px] md:mr-[50px]">
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="limitation-of-liability">Limitation of Liability</p>
+                <p class="mb-[30px]">Under no circumstances shall Oshirma! be liable for any direct, indirect, special,
+                    incidental or consequential damages, including, but not limited to, loss of data or profit, arising
+                    out of the use, or the inability to use, the materials on this site, even if Oshirma! or an
+                    authorized representative has been advised of the possibility of such damages. If your use of
+                    materials from this site results in the need for servicing, repair or correction of equipment or
+                    data, you assume any costs thereof.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="licensing-policy">Licensing Policy</p>
+                <p class="mb-[30px]">Oshirma! services and applications are released under our proprietary license
+                    agreement. Please refer to licensing policy page for licensing details.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="product-compatibility">Product Compatibility</p>
+                <p class="mb-[30px]">Our services are developed to be compatible with the latest versions of popular
+                    browsers and operating systems because we always strive to stay up-to-date with current technology
+                    standards. You might experience certain performance or functionality issues if you use outdated
+                    software or devices.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="delivery">Delivery</p>
+                <p class="mb-[30px]">Your purchased service or subscription information will be emailed to the email
+                    address (that you will provide) once we receive your payment. Even though this usually takes a few
+                    minutes, it may also take up to 24 hours. You can contact us through our contact page if you do not
+                    receive your email after waiting for this time period. You will also have access to your purchased
+                    services from your account in Oshirma! after logging in.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="ownership">Ownership</p>
+                <p class="mb-[30px]">All the products and services are the property of Oshirma!. So you may not claim
+                    ownership (intellectual or exclusive) over any of our services, modified or unmodified. Our services
+                    come 'as is', without any kind of warranty, either expressed or implied. Under no circumstances can
+                    our juridical person be accountable for any damages including, but not limited to, direct, indirect,
+                    special, incidental or consequential damages or other losses originating from the employment of or
+                    incapacity to use our services.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="browser-compatibility">Browser Compatibility</p>
+                <p class="mb-[30px]">We consider it our duty to offer a great experience across most major browsers,
+                    which is why our services support the latest modern web browsers including (but not limited to)
+                    Firefox, Safari, Chrome & Edge. However, the performance may vary between different browsers,
+                    versions, and operating systems.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="updates">Updates</p>
+                <p class="mb-[30px]">Our platform is constantly updated to provide the best experience and security.
+                    Please ensure that you always use the most current version of our application or access our website
+                    with compatible browsers.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="service-support">Service Support</p>
+                <p class="mb-[30px]">Please refer to Help and Support Policy page for details on how to get assistance
+                    with our services.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="price-changes">Price Changes</p>
+                <p class="mb-[30px]">Oshirma! reserves the right to modify or suspend (temporarily or permanently) a
+                    subscription at any point of time and from time to time with or without any notice.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="refund-policy">Refund Policy</p>
+                <p class="mb-[30px]">Please refer to Refund Policy page for details on our refund process and
+                    eligibility.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="email-and-newsletter">Email & Newsletter</p>
+                <p class="mb-[30px]">Oshirma! may occasionally send you emails related to your account, purchases, or
+                    use of our services. We may also send you occasional email newsletters relating to Oshirma! updates
+                    and promotions. We will never sell or release your email to any third party vendors. You may opt out
+                    of these emails at any time without penalty.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="license-agreement">License Agreement</p>
+                <p class="mb-[30px]">By using our services or accessing our application, you indicate and consent that
+                    you have read and agree to the Terms & Conditions listed and detailed on this page. We reserve the
+                    right to change or modify the current Terms and Conditions without prior notice or consent.</p>
+                <p class="font-bold font-chivo text-[14px] md:text-heading-6 scroll-mt-5 mb-5 text-gray-900"
+                    id="severability">Severability</p>
+                <p class="mb-[30px]">If any part of this agreement is declared unenforceable or invalid, all remaining
+                    clauses in this agreement shall remain binding on the customer.</p>
+            </div>
+            <div class="flex flex-col items-center gap-6">
+                <p class="text-heading-6 font-bold font-chivo text-gray-400">Share</p>
+                <a href="#"> <img src="/assets/images/icons/icon-fb.svg" alt="" /></a>
+                <a href="#"> <img src="/assets/images/icons/icon-twitter-black.svg" alt="" /></a>
+                <a href="#"> <img src="/assets/images/icons/icon-pi.svg" alt="" /></a>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+useHead({
+    bodyAttrs: {
+        class: "overflow-x-hidden w-screen relative terms-page"
+    }
+});
+</script>
